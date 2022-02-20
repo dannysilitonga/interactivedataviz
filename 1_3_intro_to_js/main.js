777ed3ae560b7217de4ae56641eb0399efb77f16
Danny Silitonga <dannysilitonga@yahoo.com>
2022-02-19 22:35:57 -0500

    add button to intro js

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,14 @@
 console.log('hello world');
+
+const label = document.getElementById("grocery-label")
+
+let groceryItem = 0;
+
+function onClick() {
+  groceryItem += 1
+  console.log(groceryItem)
+  console.log(typeof(groceryItem))
+  label.innerText = "Your grocery bag now has : " + String(groceryItem) + " items"
+  
+  
+}
