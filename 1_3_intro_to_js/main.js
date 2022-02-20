4b29a57265ac1bf59ae7520ceca3469a7273c10a
Danny Silitonga <dannysilitonga@yahoo.com>
2022-02-20 14:33:47 -0500

    add functionality to intro to js

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -3,12 +3,26 @@ console.log('hello world');
 const label = document.getElementById("grocery-label")
 
 let groceryItem = 0;
+let veggieItem = 0;
+let proteinItem = 0; 
 
-function onClick() {
+function printMessage() {
+  return "Your grocery bag now has : " + String(groceryItem) + " items"  
+  + " in which " + String(veggieItem)+ " are vegetables and " + String(proteinItem) 
+  + " are proteins."
+
+}
+
+function addVegetableClick() {
   groceryItem += 1
-  console.log(groceryItem)
-  console.log(typeof(groceryItem))
-  label.innerText = "Your grocery bag now has : " + String(groceryItem) + " items"
+  veggieItem += 1
+  label.innerText = printMessage()
   
-  
+}
+
+function addProteinClick(){
+  groceryItem += 1
+  proteinItem += 1
+  label.innerText = printMessage()
+   
 }
