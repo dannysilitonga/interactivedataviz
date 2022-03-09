e570e9569e42b72ff21740009b99d3ab300210c5
dannysilitonga <dsilitonga@gradcenter.cuny.edu>
2022-03-09 21:07:33 +0000

    add a distribution chart

--- 2_2_distributions/main.js
+++ 2_2_distributions/main.js
@@ -1,16 +1,56 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
-
+const width = window.innerWidth * 0.7,
+  height = window.innerHeight * 0.7,
+  margin = { top: 20, bottom: 60, left: 60, right:40 },
+  radius = 5;
 /* LOAD DATA */
-d3.json("[PATH_TO_YOUR_DATA]", d3.autoType)
-  .then(data => {
-    console.log(data)
+d3.json('../data/environmentRatings.json', d3.autoType).then(data => {
+    console.log("data", data)
 
     /* SCALES */
-    
+    /** This is where you should define your scales from data to pixel space */
+    const xScale = d3.scaleLinear()
+      .domain([0, d3.max(data.map(d => d.envScore2020))])
+      .range([margin.left, width - margin.right])
+
+    // yscale - linear, count 
+    const yScale = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.ideologyScore2020)])
+      .range([height - margin.bottom, margin.top])
+
+    const colorScale = d3.scaleOrdinal()
+      .domain(["R", "D"])
+      .range(["red", "blue", "purple"])
+
     /* HTML ELEMENTS */
+    /** Select your container and append the visual elements to it */
+    //svg
+    const svg = d3.select("#container")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+
+    // axis scales
+    const xAxis = d3.axisBottom(xScale)
+    svg.append("g")
+      .attr("transform", `translate(0, ${height - margin.bottom})`)
+      .call(xAxis);
+
+    const yAxis = d3.axisLeft(yScale)
+    svg.append("g")
+      .attr("transform", `translate(${margin.left}, 0)`)
+      .call(yAxis);
+
+    //circles       
+    const dot = svg
+      .selectAll("circle")
+      .data(data, d => d.BioID) 
+      .join("circle")
+      .attr("cx", d => xScale(d.envScore2020))
+      .attr("cy", d => yScale(d.ideologyScore2020))
+      .attr("r", radius)
+      .attr("fill", d => colorScale(d.Party))
+    console.log("the code gets to the")
     
-  });
+
+  })
