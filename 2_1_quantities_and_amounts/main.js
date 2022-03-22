196e2a40a7243da31dd6b725b1b9de67327d31df
dannysilitonga <dsilitonga@gradcenter.cuny.edu>
2022-03-22 02:02:29 +0000

    add modification to quantities and amount section

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -1,17 +1,61 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ;
-// const height = ;
+//const width = window.innerWidth *.8 ;
+//const height = 500;
+
+const margin = {top: 20, right: 30, bottom: 40, left: 90},
+    width = 800 - margin.left - margin.right,
+    height = window.innerHeight*0.8 - margin.top - margin.bottom;
+
 
 /* LOAD DATA */
-d3.csv('../[PATH_TO_YOUR_DATA]', d3.autoType)
+var svg = d3.select("#container")
+.append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+.append("g")
+  .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+
+d3.csv('../data/squirrelActivities.csv', d3.autoType)
   .then(data => {
     console.log("data", data)
 
     /* SCALES */
-    /** This is where you should define your scales from data to pixel space */
+    // xscale - categorical, activity
+    const xScale = d3.scaleLinear()
+      .domain([0, d3.max(data, d=> d.count)])   //data.map(d=> d.activity))
+      .range([0, width]); //visual variable
+      //.ticks(14);
+    
+    svg.append("g")
+      .attr("transform", "translate(0, " + height + ")")
+      .call(d3.axisBottom(xScale).ticks(14))
+      .selectAll("text")
+      .attr("transform", "translate(0,0)rotate(-45)")
+      .style("text-anchor", "end");
+
+    //yscale - linear, count
+    const yScale = d3.scaleBand()
+      .domain(data.map(d=> d.activity))
+      .range([0, height])
+      .paddingInner(0.1);
+
+    svg.append("g")
+      .call(d3.axisLeft(yScale))
     
 
-    /* HTML ELEMENTS */
+    svg.selectAll("rect")
+      .data(data)
+      .join("rect")
+      .attr("x", xScale(0))
+      .attr("y", function(d) { return yScale(d.activity); })
+      .attr("width", function(d) { return xScale(d.count); }) //)
+      .attr("height",  yScale.bandwidth() ) // d=> height - yScale(d.count))
+      .attr("fill", "#69b3a2")
+      //.attr("x", d=>xScale(d.activity))
+      //.attr("y", d=>yScale(d.count))
     /** Select your container and append the visual elements to it */
 
   })
+  
