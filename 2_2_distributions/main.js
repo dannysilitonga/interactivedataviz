6d8883aef3034953c6be29603ce10b860b903d92
dannysilitonga <dsilitonga@gradcenter.cuny.edu>
2022-03-22 05:51:08 +0000

    updated scatter plot

--- 2_2_distributions/main.js
+++ 2_2_distributions/main.js
@@ -4,23 +4,31 @@ const width = window.innerWidth * 0.7,
   margin = { top: 20, bottom: 60, left: 60, right:40 },
   radius = 5;
 /* LOAD DATA */
-d3.json('../data/environmentRatings.json', d3.autoType).then(data => {
+d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/2_TwoNum.csv", d3.autoType).then (data => {
+
+  for(i=0; i < data.length; i++){
+    data[i]["Category"] = (data[i]["SalePrice"] > 250000) ? "true" : "false"
+  }
+
+  //data["category"] = (data.map((d) => ["SalePrice"] > 400000) ? "true" : "false")
+//d3.json('../data/environmentRatings.json', d3.autoType).then(data => {
     console.log("data", data)
+    //console.log(data["Category"])
 
     /* SCALES */
     /** This is where you should define your scales from data to pixel space */
     const xScale = d3.scaleLinear()
-      .domain([0, d3.max(data.map(d => d.envScore2020))])
+      .domain([0, d3.max(data.map(d => d.GrLivArea))])
       .range([margin.left, width - margin.right])
 
     // yscale - linear, count 
     const yScale = d3.scaleLinear()
-      .domain([0, d3.max(data, d => d.ideologyScore2020)])
+      .domain([0, d3.max(data, d => d.SalePrice)])
       .range([height - margin.bottom, margin.top])
 
     const colorScale = d3.scaleOrdinal()
-      .domain(["R", "D"])
-      .range(["red", "blue", "purple"])
+      .domain(data.map((d) => d["Category"]))
+      .range(["red", "purple"])
 
     /* HTML ELEMENTS */
     /** Select your container and append the visual elements to it */
@@ -42,22 +50,22 @@ d3.json('../data/environmentRatings.json', d3.autoType).then(data => {
       .call(yAxis);
 
     //circles       
-    const dot = svg
+    svg
       .selectAll("circle")
-      .data(data, d => d.BioID) 
+      .data(data) 
       .join(
         enter => enter
           .append("circle")
           .attr("r", 1)
-      .attr("cx", d => xScale(d.envScore2020))
-      .attr("cy", d => yScale(d.ideologyScore2020))
-      .attr("r", radius)
-      .attr("fill", "black")
-        .transition()
-        .duration(4000) // in ms
-        .delay(200)
-          .attr("r", 10)
-          .attr("fill", d=> colorScale(d.Party))
+          .attr("cx", d => xScale(d.GrLivArea))
+          .attr("cy", d => yScale(d.SalePrice))
+          //.attr("r", radius)
+          .attr("fill", "red")
+            .transition()
+            .duration(4000) // in ms
+            .delay(200)
+            .attr("r", 5)
+            .attr("fill", d=> colorScale( d.Category ))
       )
 
   });
